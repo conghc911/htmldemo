457fa983343e57495145413fa1e7b995ac92b94c
conghc911 <conghc911@gmail.com>
2023-11-12 10:07:26 +0700

    add html lap

--- js/index.js
+++ js/index.js
@@ -1,51 +1,92 @@
-$(document).ready(function(){
-	// slide
-	
-
-	$('.slider-home').owlCarousel({
-		loop: true,
-		margin: 10,
-	    autoplay:true,
-	    dots: false,
-	    nav:false,
-	    navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-	    responsive: {
-	    	0: {
-	    		items: 1
-	    	},
-	    	600: {
-	    		items: 1
-	    	},
-	    	1000: {
-	    		items: 1
-	    	}
-	    }
-	});
-	
-	$(".slide-prd-sidebar").slick({
-	  dots: true,
-	  vertical: true,
-	  centerMode: true,
-	  slidesToShow: 2,
-	  slidesToScroll: 2,
-	});
-	
+$(document).ready(function() {
+    // slide
 
 
-	$(".mobile").click(function(){
-		$(".menu-nav").toggleClass('open');
-	});
-	$("span.book_close").click(function(){
-		$("div#menu-tab").removeClass("open");
-		$("div.book_overlay").css("display","none");
-	});
-	
+    // $('.slider-home').owlCarousel({
+    // 	loop: true,
+    // 	margin: 10,
+    //     autoplay:true,
+    //     dots: false,
+    //     nav:false,
+    //     navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+    //     responsive: {
+    //     	0: {
+    //     		items: 1
+    //     	},
+    //     	600: {
+    //     		items: 1
+    //     	},
+    //     	1000: {
+    //     		items: 1
+    //     	}
+    //     }
+    // });
+
+    $('.slider-td').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        dots: false,
+        nav: false,
+        navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
+    $('.slider-lh').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        dots: false,
+        nav: false,
+        navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
+
+    $(".slide-prd-sidebar").slick({
+        dots: true,
+        vertical: true,
+        centerMode: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+    });
+
+
+
+    $(".mobile").click(function() {
+        $(".menu-nav").toggleClass('open');
+    });
+    $("span.book_close").click(function() {
+        $("div#menu-tab").removeClass("open");
+        $("div.book_overlay").css("display", "none");
+    });
+
 
 });
 
 var aboveHeight = $('#topheader').outerHeight();
 console.log(aboveHeight);
-$(window).scroll(function () {
+$(window).scroll(function() {
     if ($(window).scrollTop() > aboveHeight) {
         $('div#menu').addClass('fixed');
 
@@ -53,4 +94,3 @@ $(window).scroll(function () {
         $('div#menu').removeClass('fixed');
     }
 });
-
