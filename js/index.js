a3be9cedeeb8c1ef1534f7e6b2735a7378a492a6
Huy Công <conghc911@gmail.com>
2023-11-12 20:56:12 +0700

    donebase

--- js/index.js
+++ js/index.js
@@ -2,25 +2,25 @@ $(document).ready(function() {
     // slide
 
 
-    // $('.slider-home').owlCarousel({
-    // 	loop: true,
-    // 	margin: 10,
-    //     autoplay:true,
-    //     dots: false,
-    //     nav:false,
-    //     navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-    //     responsive: {
-    //     	0: {
-    //     		items: 1
-    //     	},
-    //     	600: {
-    //     		items: 1
-    //     	},
-    //     	1000: {
-    //     		items: 1
-    //     	}
-    //     }
-    // });
+    $('.slider-lh-s').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        dots: false,
+        nav: false,
+        navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
 
     $('.slider-td').owlCarousel({
         loop: true,
